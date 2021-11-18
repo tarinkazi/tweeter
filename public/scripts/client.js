576808ed6a43c617f6cfa7e99ba1b37893464320
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-17 23:57:23 -0500

    rendering data dynamic and add timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 
 const tweetData = {
   "user": {
@@ -17,7 +42,12 @@ const tweetData = {
 }
 
 
-
+function renderTweets(tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweets);
+    $('#tweet-history').append($tweet);
+  }
+}
 // Test / driver code (temporary)
 
 
@@ -40,7 +70,7 @@ const createTweetElement = function(data) {
 </div>
 
 <footer>
-<span class="time-passed">${created_at}</span>
+<span class="time-passed">${timeago.format(created_at)}</span>
 <div class='icons'>
   <i class="fas fa-flag"></i>
   <i class="fas fa-retweet"></i>
@@ -52,6 +82,12 @@ const createTweetElement = function(data) {
 
 }
 
-const $tweet = createTweetElement(tweetData);
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); 
+// const $tweet = createTweetElement(tweetData);
+// console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); 
+
+$(document).ready(function() {
+  renderTweets(data);
+
+
+});
