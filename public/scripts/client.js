356304a5d6b094165ee2753debe98573bf438e1c
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-17 19:38:41 -0500

    try to pass object in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,39 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+
+
+// Test / driver code (temporary)
+
+
+const createTweetElement = function(data) {
+  console.log("DATA IS INSIDE");
+    console.log(data)
+    const item = `
+    <div class="item">
+        <img src="${data.user.avatars}" />
+        <h2>${data.content}</h2>
+        <h4>${data.created_at}</h4>
+    </div>`
+
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+ // console.log($tweet);
+ return item;
+
+}
+
+const $tweet = createTweetElement(tweetData);
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); 
